9d953cb3b3cc98e84fe0b3fabe40d75fa0ddd2f0
haradakyouhei <harada-kyohei-fj@ynu.jp>
2020-10-24 22:58:57 +0900

    Joined ARC106

---
+++ ARC106/q1.py
@@ -0,0 +1,44 @@
+import sys
+from io import StringIO
+import unittest
+
+
+def resolve():
+    n = int(input())
+    for a in range(1, 40):
+        for b in range(1, 40):
+            if 3**a + 5**b == n:
+                print(a, b)
+                return
+
+    print(-1)
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """106"""
+        output = """4 2"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """1024"""
+        output = """-1"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """10460353208"""
+        output = """21 1"""
+        self.assertIO(input, output)
+
+
+if __name__ == "__main__":
+    unittest.main()
