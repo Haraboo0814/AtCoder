ffd914289e11cd3694fcb4601dfd747a1cce48ad
haradakyouhei <harada-kyohei-fj@ynu.jp>
2020-11-16 00:07:56 +0900

    aribook

---
+++ AriBook_Python/2_5_Find_Negative_Loop.py
@@ -0,0 +1,33 @@
+class Edge:
+    def __init__(self, n_from, n_to, cost):
+        self.n_from = n_from
+        self.n_to = n_to
+        self.cost = cost
+
+
+es = []
+MAX_V = 100000
+MAX_E = 100000
+INF = 10**8
+
+V, E = map(int, input().split())
+for i in range(E):
+    f, t, c = map(int, input().split())
+    es.append(Edge(f, t, c))
+    es.append(Edge(t, f, c))
+d = [0 for i in range(V)]
+
+
+def find_negative_loop(V):
+    for i in range(V):
+        for e in es:
+            if d[e.n_to] > d[e.n_from] + e.cost:
+                d[e.n_to] = d[e.n_from] + e.cost
+
+        if i == V - 1:
+            return True
+
+    return False
+
+
+print(find_negative_loop(V))
